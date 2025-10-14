1200cc4c2bd93374141388238c4e14d2c2dbac78
ColeUyematsu <cjuyematsu@gmail.com>
2025-10-14 15:12:10 -0700

    initial commit

---
+++ src/main/java/com/ccm/tuunapi/TuunApiApplication.java
@@ -0,0 +1,13 @@
+package com.ccm.tuunapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuunApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuunApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ccm/tuunapi/TuunApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccm.tuunapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuunApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
